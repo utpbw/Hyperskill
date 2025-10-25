1081248d919dc84a16443ffa419c8c13ef21d072
Yaraslau <yaraslau.krautsou@icloud.com>
2025-10-26 01:20:01 +0200

    Add tasks endpoint returning OK

---
+++ Challenging/Task Management System/TaskController.java
@@ -0,0 +1,16 @@
+package com.example.accounts.api;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/tasks")
+public class TaskController {
+
+    @GetMapping
+    public ResponseEntity<?> listTasks() {
+        return ResponseEntity.ok().build();
+    }
+}
